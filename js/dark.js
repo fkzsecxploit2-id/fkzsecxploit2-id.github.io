88ae77a2163cc6a95f3bf43111490c4af9858903
fkzsecxploit.dev <fkzsec46@gmail.com>
2024-12-27 17:56:33 +0700

    Create dark.js

---
+++ js/dark.js
@@ -0,0 +1,7 @@
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const body = document.body;
+
+darkModeToggle.addEventListener('change', () => {
+    body.classList.toggle('dark-mode', darkModeToggle.checked);
+});
+
