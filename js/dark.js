8fe417358be160912983a81a4423c794d6ec4b9d
fkzsecxploit.dev <fkzsec46@gmail.com>
2025-03-03 01:09:08 +0700

    Delete js directory

--- js/dark.js
+++
@@ -1,7 +0,0 @@
-const darkModeToggle = document.getElementById('dark-mode-toggle');
-const body = document.body;
-
-darkModeToggle.addEventListener('change', () => {
-    body.classList.toggle('dark-mode', darkModeToggle.checked);
-});
-
